cd7a61df769bfcd6afd689a0d323d8784c8a9f5b
KarateJB <ppippenkimo@gmail.con>
2017-09-25 14:58:17 +0800

    Create web service

---
+++ src/MVC/Mvc517/Mvc517.Website/Webservice/WsSysInfo.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="WsSysInfo.asmx.cs" Class="Mvc517.Website.Webservice.WsSysInfo" %>
---
+++ src/MVC/Mvc517/Mvc517.Website/Webservice/WsSysInfo.asmx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Script.Serialization;
+using System.Web.Script.Services;
+using Mvc517.DAL.Models;
+
+namespace Mvc517.Website.Webservice
+{
+    /// <summary>
+    /// Summary description for SysInfo
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WsSysInfo : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public string Get()
+        {
+            var sys = new SysInfo()
+            {
+                Title = "UUU.MVC517",
+                Year = 2017,
+                Author = "JB.Lin"
+            };
+            return new JavaScriptSerializer().Serialize(sys);
+        }
+    }
+}
