19bd3225953724585194114f902bf2b29f62134d
KarateJB <ppippenkimo@gmail.con>
2017-09-27 14:06:55 +0800

    Complete webapi methods

--- src/MVC/Mvc517/Mvc517.Website/Controllers/RestApiController.cs
+++ src/MVC/Mvc517/Mvc517.Website/Controllers/RestApiController.cs
@@ -1,39 +1,79 @@
-﻿using System;
+﻿using Mvc517.DAL;
+using Mvc517.DAL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace Mvc517.Website.Controllers
 {
     public class RestApiController : ApiController
     {
-        // GET: api/RestApi
-        public IEnumerable<string> Get()
+        [HttpGet]
+        public async Task<IEnumerable<Opera>> Get()
         {
-            return new string[] { "value1", "value2" };
+            using (var dbContext = new MvcDbContext())
+            {
+                return dbContext.Operas.ToList();
+            }
         }
 
-        // GET: api/RestApi/5
-        public string Get(int id)
+
+        [HttpPost]
+        public async Task<HttpResponseMessage> Create(Opera viewModel)
         {
-            return "value";
+            if (!ModelState.IsValid)
+            {
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            }
+
+
+            using (var dbContext = new MvcDbContext())
+            {
+                viewModel.CreateOn = DateTime.Now;
+                viewModel.UpdateOn = DateTime.Now;
+                dbContext.Operas.Add(viewModel);
+                await dbContext.SaveChangesAsync();
+            }
+
+            return new HttpResponseMessage(HttpStatusCode.Created);
         }
 
-        // POST: api/RestApi
-        public void Post([FromBody]string value)
+        [HttpPut]
+        public async Task<HttpResponseMessage> Edit(Opera viewModel)
         {
+            if (!ModelState.IsValid)
+            {
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+            }
+
+            using (var dbContext = new MvcDbContext())
+            {
+                viewModel.UpdateOn = DateTime.Now;
+                dbContext.Entry(viewModel).State = System.Data.Entity.EntityState.Modified;
+
+                await dbContext.SaveChangesAsync();
+            }
+
+            return new HttpResponseMessage(HttpStatusCode.OK);
         }
 
-        // PUT: api/RestApi/5
-        public void Put(int id, [FromBody]string value)
+        
+        [HttpDelete]
+        public async Task<HttpResponseMessage> Delete(int id)
         {
-        }
 
-        // DELETE: api/RestApi/5
-        public void Delete(int id)
-        {
+            using (var dbContext = new MvcDbContext())
+            {
+                var entity = dbContext.Operas.Where(x => x.Id.Equals(id)).FirstOrDefault();
+                dbContext.Operas.Remove(entity);
+                await dbContext.SaveChangesAsync();
+            }
+
+            return new HttpResponseMessage(HttpStatusCode.OK);
         }
     }
 }
---
+++ src/MVC/Mvc517/Mvc517.Website/Controllers/RestController.cs
@@ -0,0 +1,86 @@
+﻿using Mvc517.DAL;
+using Mvc517.DAL.Models;
+using Mvc517.Utility;
+using Mvc517.Website.Filter;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc517.Website.Controllers
+{
+    //[HandleError(View ="Error")]
+    public class RestController : Controller
+    {
+        MvcDbContext _dbContext = null;
+
+        protected override void OnException(ExceptionContext filterContext)
+        {
+            filterContext.Result = View("Error");
+        }
+
+
+        public RestController()
+        {
+            this._dbContext = new MvcDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            this._dbContext.Dispose();
+        }
+
+
+        
+        public ActionResult Create(string myName)
+        {
+            //Debug.WriteLine($"myName");
+            var viewModel = new Opera();
+            return View(viewModel);
+        }
+
+        
+
+        public ActionResult Edit(int? id)
+        {
+            if (!id.HasValue)
+                return HttpNotFound();
+
+
+            using (var dbContext = new MvcDbContext())
+            {
+                var viewModel = dbContext.Operas.Where(x => x.Id.Equals(id.Value)).FirstOrDefault();
+
+                return View(viewModel);
+            }
+
+        }
+
+
+        
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="myName"></param>
+        /// <returns></returns>
+        [LogFilter]
+        public ActionResult Index()
+        {
+            using (var dbContext = new MvcDbContext())
+            {
+                var viewModel = dbContext.Operas.ToList();
+                return View(viewModel);
+            }
+        }
+
+
+
+
+    }
+}
